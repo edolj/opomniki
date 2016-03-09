01d92232c63f11fc0dd6fb2e120d7a5270a58a6c
Edo <edo.ljubijankic@hotmail.com>
2016-03-09 15:20:29 +0000

    Implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -11,10 +11,26 @@ window.addEventListener('load', function() {
 			var cas = parseInt(casovnik.innerHTML);
 	
 			//TODO: 
+			if(cas == 0) {
+				console.log("Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!");
+			} else {
+				cas--;
+				
+			}
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
+    
+    var izvediPrijavo = function(event) {
+		var uporabnisko_ime=document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = uporabnisko_ime;
 	
+		document.querySelector(".pokrivalo").style.visibility = "hidden";
+		
+ 	}
+	
+	document.querySelector("#prijavniGumb").addEventListener('click',izvediPrijavo);  
+    
 });
